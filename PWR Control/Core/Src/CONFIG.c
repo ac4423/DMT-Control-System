e8d494db3a0d7ca2046e3dfcd024e783ce8f45a1
andrei-chernolesskiy <ac4423@ic.ac.uk>
2026-01-23 00:47:32 +0000

    Revert "Comit 3"
    This reverts commit ccba999621a607520eeff8e30ee09e5c5e248320.

--- PWR Control/Core/Src/CONFIG.c
+++ PWR Control/Core/Src/CONFIG.c
@@ -9,8 +9,13 @@
 #define MS_TO_TICKS(ms)   ((uint32_t)(((ms) * 1000U) / TIM6_TICK_uS))
 
 uint32_t flowmeter_window_ticks = MS_TO_TICKS(FLOW_WINDOW_MS);
-uint32_t serial_send_ticks_threshold      = MS_TO_TICKS(SERIAL_SEND_MS);
+uint32_t serial_send_ticks_threshold = MS_TO_TICKS(SERIAL_SEND_MS);
 
-void flags_init(void) {
-	usb_serial_flag = 0;
+void flags_init(void)
+{
+    flowmeter_window_ticks =
+        (FLOW_WINDOW_MS * 1000UL) / TIM6_TICK_uS;
+
+    serial_send_ticks_threshold =
+        (SERIAL_SEND_MS * 1000UL) / TIM6_TICK_uS;
 }
